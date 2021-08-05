d4457a0c558fbf6b085d8bf500456348cfeee1fc
lekshmi <lekshmirajendran123@gmail.com>
2021-08-05 13:36:13 +0530

    Lekshmi updated a file

--- Lekshmi_Pillai_185386/menucar.sh
+++ Lekshmi_Pillai_185386/menucar.sh
@@ -10,22 +10,41 @@ read choice
 
 case $choice in 
 	'1') 
-	     IFS= $'\n'
-	     files=`cat carss.csv | cut -d "," -f14,15`
-	     for i in $files
-	     do 
-	     echo $i
-	     	#make=`echo $i | cut -d ":" -f1`
-	     	#make=`echo $make | cut -d '"' -f2`
-		#id=`echo $i | cut -d ":" -f2`
-		#echo $make
-		#echo $id
-	     	#if(( $make == Audi ))
-	     	#then echo "Audoo"
-	     	#fi
+	     cat carss.csv | while read l
+	     do
+	     	make=`echo $l | cut -d "," -f14  | cut -d '"' -f2`
+	     	echo $make
+	     	if(( $make == "Audi" ))
+	     	then echo $l
+	     	fi
 	     done
 	     	;;
+	 '2') echo "Read gear input"
+	      read gear
+	      cat carss.csv | while read l
+	      do
+	     	make=`echo $l | cut -d "," -f7  | cut -d '"' -f2`
+	      if(( $make < $gear ))
+	     	then echo $l
+	      else
+	        echo "Enter valid gear"
+	     	fi
+	     done
+	     	;;
+	  '3') echo "Dimension Height and width"
+	       read height
+	       read width
+	       cat carss.csv | while read l
+	       do
+	     	heightt=`echo $l | cut -d "," -f1  | cut -d '"' -f2`
+	     	widthh=`echo $l | cut -d "," -f3  | cut -d '"' -f2`
+	     	echo $heightt
+	     	if(( $heightt > $height &&  $widthh > $width ))
+	     	then echo $l
+	     	fi
+	      done
+	     	;;
 	     
-	 *) ;;
+	 *) echo "Enter valid choice";;
 esac
 
