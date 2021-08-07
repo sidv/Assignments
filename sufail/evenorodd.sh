8260e7c7e3956dd6852b7226a8889aca1938844d
sufail <sufailps786@gmail.com>
2021-08-07 12:33:01 +0530

    adding assignment files

---
+++ sufail/evenorodd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "Enter a number to check for even or odd"
+read num
+
+if (( num == 0 ))
+then
+echo	"$num is not even nor odd"
+
+elif (( num % 2 == 0  ))
+then
+	echo "$num is an even number"
+else
+	echo "$num is an odd number"
+fi
---
+++ sufail/managefiles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Menu Driven Programming"
+echo "1. Create Files"
+echo "2. Delete Files"
+echo "3. Enter your name"
+
+echo "Enter your choice"
+read choice
+
+case $choice in 
+	"1") echo "Enter input"
+	     read n
+	     for ((i=1;i<=n;i++))
+	     do
+	     	touch $i".txt" 
+	     done
+	     	;;
+	"2") echo "Which file to be delete?"
+	     read n
+	      rm $n".txt"
+	     ;;
+	"3") echo "Enter your name"
+	     cat > names.txt
+	     ;;
+	"*") echo "Invalid Number"
+             ;;
+esac
