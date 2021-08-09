f8c4ee210af11342da6cd053707b79217d2ea7bc
thanushree <thanushreethanu28@gmail.com>
2021-08-09 11:03:11 +0530

    updatd file

---
+++ Thanushree/Assignment/cancer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+lines=`cat cancer.csv | wc -l`
+cat cancer.csv | tail -n $(( lines-1 )) | while read line
+do 	
+	del=`echo $line | cut -d"," -f4 | tr -d '"'`
+	if [[ ${del%.*} -gt 10000000 ]]
+	then
+		c=`echo $line | cut -d"," -f2 | tr -d '"'`
+		if [[ ${c%.*} -gt 200 ]]
+		then
+			echo $line | cut -d"," -f1 | tr -d '"'
+		fi
+	fi
+done
