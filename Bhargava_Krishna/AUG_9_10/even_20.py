65d5dc280e47476c93d9f4fcddd47c2ad3389e16
Bhargava <bhargavdependable@gmail.com>
2021-08-12 00:50:05 +0530

    Assignments .py files

---
+++ Bhargava_Krishna/AUG_9_10/even_20.py
@@ -0,0 +1,4 @@
+for i in range(1,21):
+
+		if i%2 == 0:
+			print(i)
---
+++ Bhargava_Krishna/AUG_9_10/str_add.py
@@ -0,0 +1,9 @@
+x = "1,2,3,4,5,6,7,8,9"
+
+a = x[0::2]
+print(a)
+
+b =int(a[1]) + int(a[2]) + int(a[3]) + int(a[4]) + int(a[5]) + int(a[6]) + int(a[7])
+
+
+print(b)
