82690337dc2cc7f20f6a2fa58ce9d85b985cab6d
Anjitha <184491@ust.com>
2021-08-13 00:25:18 +0530

    Assignments of Aug11(optional)

---
+++ anjitha/Aug11/basicop4.py
@@ -0,0 +1,12 @@
+
+
+print("Fibonnacim Series!!!")
+first=0
+second=1
+print(second)
+for i in range(101):
+	third=first + second
+	print(third)
+	first=second
+	second=third
+	
---
+++ anjitha/Aug11/basicop5.py
@@ -0,0 +1,10 @@
+
+
+
+print("To check palindrome or not!!")
+num=input("Enter a String!!")
+t=num[::-1]
+if t == num:
+	print("The given string is Palindrome!!")
+else:
+	print("The given string is not palindrome!!!")
