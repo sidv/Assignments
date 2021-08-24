475a15a9223d1bd44dd2faf229dab0fb373ea867
Bhargava <bhargavdependable@gmail.com>
2021-08-25 01:59:42 +0530

    comprehension

---
+++ Bhargava_Krishna/day_24_comprehension/gen_char.py
@@ -0,0 +1,18 @@
+
+lst = ["Siddhant","Pavan","Ramya","Raja"]
+
+
+y = list(map(lambda a : a[0:3] , lst))
+print(y)	
+	
+print([x[0:3] for x in ["Siddhant","Pavan","Ramya","Raja"]])
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
