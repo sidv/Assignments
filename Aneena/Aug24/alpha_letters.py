8c6d92eb136f6b2adca36a019d64ee2e9531b8de
anee <anee1234@gmail.com>
2021-08-29 22:42:19 +0530

    Assigments of Aug23,24

---
+++ Aneena/Aug24/alpha_letters.py
@@ -0,0 +1,6 @@
+#program using os module to create all alphabetical letters file
+#E.g filename should be a.txt , b.txt and so on)(Use os modul
+import os
+string="abcdefghijklmnopqrstuvwxyz"
+for i in string:
+	os.system(f"touch {i}.txt")
