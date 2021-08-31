24467224f56faa6921081a91fb7454ab349054dc
hyraja <paikaray.milan@gmail.com>
2021-08-31 15:57:16 +0530

    Day22 assignments added

---
+++ Rajaprasad/Day22-assignments/employee.py
@@ -0,0 +1,10 @@
+class Emp:
+    def __init__(self, emp_id=0, fname='', lname='', age=0, gender='', salary='', pre_comp='', join_date=''):
+        self.emp_id = emp_id
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.gender = gender
+        self.salary = salary
+        self.pre_comp = pre_comp
+        self.join_date = join_date
