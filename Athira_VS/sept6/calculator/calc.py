0faed3e3fe51af3760be0d3bf99fac7026fdbab4
athira <vsathira33@gmail.com>
2021-09-17 16:51:50 +0530

    Assignments of September 6.

---
+++ Athira_VS/sept6/calculator/calc.py
@@ -0,0 +1,24 @@
+class Calc:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def subtract(self):
+        return self.a - self.b
+
+    def multiply(self):
+        return self.a * self.b
+
+    def divide(self):
+        return self.a / self.b
+
+    def floor_divide(self):
+        return self.a // self.b
+
+    def modulo_divide(self):
+        return self.a % self.b
+
